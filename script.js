775ef513a92cae851c89edacef77e4ec223b0f83
omxpal <ompal12.2004@gmail.com>
2024-07-19 11:41:41 +1000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF3388'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
-document.getElementById('colorButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#ffc0cb';
-    this.style.display = 'none';
-});
