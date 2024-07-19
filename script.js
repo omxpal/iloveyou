347fa0cee536d8ab55027c7c7070c63fa4ae450a
omxpal <ompal12.2004@gmail.com>
2024-07-19 11:28:44 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF3388'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
